801497ba2fab2e7260ec2b1d7ddb4597ac156dbd
lomatik <ralenko2012@gmail.com>
2021-11-19 22:28:19 +0200

    testcode from exmp.

--- main.py
+++ main.py
@@ -76,5 +76,36 @@ def backward_propagate_error(network, expected):
             neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
 
 
+def update_weights(network, row, l_rate):
+    for i in range(len(network)):
+        inputs = row[:-1]
+        if i != 0:
+            inputs = [neuron['output'] for neuron in network[i - 1]]
+        for neuron in network[i]:
+            for j in range(len(inputs)):
+                neuron['weights'][j] -= l_rate * neuron['delta'] * inputs[j]
+            neuron['weights'][-1] -= l_rate * neuron['delta']
+
+
+def train_network(network, train, l_rate, n_epoch, n_outputs):
+    for epoch in range(n_epoch):
+        sum_error = 0
+        for row in train:
+            outputs = forward_propagate(network, row)
+            expected = [0 for i in range(n_outputs)]
+            expected[row[-1]] = 1
+            sum_error += sum([(expected[i] - outputs[i]) ** 2
+                              for i in range(len(expected))])
+            backward_propagate_error(network, expected)
+            update_weights(network, row, l_rate)
+        print('>epoch=%d, lrate=%.3f, error=%.3f' %
+              (epoch, l_rate, sum_error))
+
+
+def predict(network, row):
+    outputs = forward_propagate(network, row)
+    return outputs.index(max(outputs))
+
+
 testcsv = pd.read_csv('house-prices-advanced-regression-techniques/test.csv');
 traincsv = pd.read_csv('house-prices-advanced-regression-techniques/train.csv');
